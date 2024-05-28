5e265be9134e130692a9c9395f15f90c8f07cfcf
Anki <meriidmerihihe@gmail.com>
2024-05-28 10:45:39 +0530

    new folder problem ok

--- src/HOC&Context/Provider/UserAccountProvider.tsx
+++ src/HOC&Context/Provider/UserAccountProvider.tsx
@@ -1,5 +1,9 @@
 import React, { useEffect, useState, ReactNode } from "react";
-import { AlertShowerProviderHOC, FolderProviderHOC, UserAccountProvider } from "../Context";
+import {
+  AlertShowerProviderHOC,
+  FolderProviderHOC,
+  UserAccountProvider,
+} from "../Context";
 import { GetFolders, GetUserName, LogInApi, SignUpApi } from "../../ApiCall";
 import { Folder } from "./FolderInfoProvider";
 import { generateRandomString } from "../../Body/RandomStr";
@@ -25,11 +29,11 @@ const UserProvider: React.FC<UserProviderProps> = ({
 }) => {
   const sttoken = localStorage.getItem("token") || "";
 
-  const [ token, setToken ] = useState(sttoken) 
+  const [token, setToken] = useState(sttoken);
   const [user, setUser] = useState<string | null>(null);
 
   useEffect(() => {
- ( token !== '' && localStorage.setItem("token", token))
+    token !== "" && localStorage.setItem("token", token);
     if (token) {
       GetUserName()
         .then((response) => {
@@ -37,8 +41,8 @@ const UserProvider: React.FC<UserProviderProps> = ({
           setLoading(false);
         })
         .catch((err) => {
-          if(err.message !== "Network error"){
-           setToken('')
+          if (err.message !== "Network error") {
+            setToken("");
             localStorage.removeItem("token");
           }
           if (err.data === "User not found") {
@@ -64,50 +68,57 @@ const UserProvider: React.FC<UserProviderProps> = ({
         setShowAlert({
           value: showAlert.value + 1,
           type: "success",
-          message: type === "signup"? "User created successfully" : "User logged in successfully",
+          message:
+            type === "signup"
+              ? "User created successfully"
+              : "User logged in successfully",
         });
-        GetFolders(data.data.token).then((r) => {
-          console.clear();
-          setFolders(r.data);
+        GetFolders(data.data.token)
+          .then((r) => {
+            console.clear();
+            setFolders(r.data);
 
-          setLoading(false);
-        }).catch((e) => {
+            setLoading(false);
+          })
+          .catch((e) => {
+            if (e.message === "Request failed with status code 404") {
+              const folderId = generateRandomString(19);
 
-          if (e.message === "Request failed with status code 404") {
-            setFolders({
-              "example folder": {
-                id: generateRandomString(19),
-              },
-            });
-          }
-          setShowAlert({
-            value: showAlert.value + 1,
-            type: "error",
-            message: e.message || e.data,
-          });
-        })
-
-        
-      })
-      .catch((err) => {
-        setLoading(false);
-        if (err.data === "User already exists"){
+              localStorage.setItem("folder" + folderId, "new");
+              setFolders({
+                "example folder": {
+                  id: folderId,
+                },
+              });
+            }
             setShowAlert({
               value: showAlert.value + 1,
               type: "error",
-              message: "User already exists",
+              message: e.message || e.data,
             });
-            return;
-  
-        }
-          if (err.message === "Request failed with status code 400") {
-            err.message =  type == "login" ? " email or password is incorrect" : "User already exists";
-          }
+          });
+      })
+      .catch((err) => {
+        setLoading(false);
+        if (err.data === "User already exists") {
           setShowAlert({
             value: showAlert.value + 1,
             type: "error",
-            message: err.message || err.data,
+            message: "User already exists",
           });
+          return;
+        }
+        if (err.message === "Request failed with status code 400") {
+          err.message =
+            type == "login"
+              ? " email or password is incorrect"
+              : "User already exists";
+        }
+        setShowAlert({
+          value: showAlert.value + 1,
+          type: "error",
+          message: err.message || err.data,
+        });
       });
   };
 
